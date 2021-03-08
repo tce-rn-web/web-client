b94c2ca7019af99f83fd98e803c58cc73709ac58
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2021-03-08 08:40:51 -0300

    Correção

--- src/models/pedido_prato.model.ts
+++ src/models/pedido_prato.model.ts
@@ -6,14 +6,14 @@ export class PedidoPrato {
     constructor(
         public pedido?: Pedido,
         public pedidoId?: number,
-        //public prato?: Prato,
+        public prato?: Prato,
         public pratoId?: number,
         public quantidade?: number,
     ) {
         this.pedido = null
         this.pedidoId = pedidoId || this.pedido?.id || null
         this.quantidade = quantidade || 1
-        //this.prato = prato || null
+        this.prato = prato || null
         this.pratoId = pratoId || 0
     }
 }
